ee81e738b4e5e715d7c804d33ade84b4ca4473ed
rameshdhv <75783184+rameshdhv@users.noreply.github.com>
2022-11-19 21:00:08 +0530

    custom tools v2.0

--- customtools_v1.0.py
+++ customtools_v1.0.py
@@ -90,7 +90,108 @@ def controlatpivot(*args):
         ctrl_grp=cmds.group(ctrl,n=i+"Ctrl_Group")
         ctrllocation=cmds.xform(ctrl_grp,ws=1,t=pointlocation)
         cmds.parent(jnt_grp,ctrl)
-##################################################################################        
+################################################################################## 
+
+def follicelcontrol(*args):
+
+    
+    
+    sel=cmds.ls(sl=1,fl=1)
+    if len(sel)==0:
+        cmds.error("please select the nurbsSurface")
+    else:
+        for i in sel:
+            
+            #planes=cmds.nurbsPlane(i,ax=[0,1,0],lr=1,w=0.2,ch=0,n=i+"_Follicle_Geo")
+            
+            plane_shapes=cmds.pickWalk(sel[0],d="down")
+            
+            follicle_shapes = cmds.createNode("follicle",n=i+"_Follicle")
+            print(follicle_shapes)
+            #transform_node=cmds.createNode("transform",n=i+"_Transform")
+            follicle= cmds.pickWalk(follicle_shapes,d="up")
+            print(follicle)
+            #query the world position
+            pos=cmds.xform(i,q=True,ws=1,t=1)
+            cmds.xform(sel[0],t=pos)
+            
+            
+            
+            cmds.connectAttr(plane_shapes[0]+".local",follicle_shapes+".inputSurface")
+            cmds.connectAttr(plane_shapes[0]+".worldMatrix[0]",follicle_shapes+".inputWorldMatrix")
+            cmds.connectAttr(follicle_shapes+".outRotate",follicle[0] +".rotate")
+            cmds.connectAttr(follicle_shapes+".outTranslate",follicle[0] +".translate")
+            cmds.setAttr(follicle_shapes+".parameterU",0.5)
+            cmds.setAttr(follicle_shapes+".parameterV",0.5)
+            
+            #create and snap the control group to xform
+            
+            circle_ctrl = cmds.circle(ch=0)
+            sdk_grp = cmds.group(circle_ctrl,n=circle_ctrl[0]+"_SDK_Group")
+            print(sdk_grp)
+            ctrl_grp =  cmds.group(sdk_grp,n=circle_ctrl[0]+"_Ctrl_Group")
+            cmds.xform(ctrl_grp,ws=1,t = pos)    
+            
+            #parent constrain ctrl group with the new follicle
+            
+            cmds.parentConstraint(follicle,ctrl_grp)
+                
+            #create a PlusminusAverage node and make the respective connections 
+            pma=cmds.createNode("plusMinusAverage",n=i+"_PMA")
+            
+            MD01=cmds.createNode("multiplyDivide",n=i+"_neg_MD")
+            MD02=cmds.createNode("multiplyDivide",n=i+"MD_offset")
+           
+            #extra attributes for the follicle control
+            cmds.addAttr(circle_ctrl[0], ln = "X_senstivity",  at='float',  min=0, dv=0.1, k=1)
+            cmds.addAttr(circle_ctrl[0], ln = "Y_senstivity",  at='float',  min=0, dv=0.1, k=1)
+            
+            cmds.addAttr(circle_ctrl[0], ln = "X_Offset",  at='float', dv=0.1, k=1)
+            cmds.addAttr(circle_ctrl[0], ln = "Y_Offset",  at='float', dv=0.1, k=1)
+            
+            #use a offsetMD node to control the sensitivity of controls
+            cmds.connectAttr(circle_ctrl[0]+".translateX",MD02+".input1X")
+            cmds.connectAttr(circle_ctrl[0]+".translateY",MD02+".input1Y")
+            
+            cmds.connectAttr(circle_ctrl[0]+".X_senstivity",MD02+".input2X")
+            cmds.connectAttr(circle_ctrl[0]+".Y_senstivity",MD02+".input2Y")
+            
+            
+            
+            #cmds.setAttr(MD02+".outputX",
+            
+            
+            
+            cmds.connectAttr(MD02+".outputX",pma+".input2D[1].input2Dx")
+            cmds.connectAttr(MD02+".outputY",pma+".input2D[1].input2Dy")
+            #xoffset and y offset connections
+            
+            cmds.connectAttr(circle_ctrl[0]+".X_Offset",pma+".input2D[2].input2Dx")
+            cmds.connectAttr(circle_ctrl[0]+".Y_Offset",pma+".input2D[2].input2Dy")
+            
+            cmds.setAttr(pma+".input2D[0].input2Dx",0.5)
+            cmds.setAttr(pma+".input2D[0].input2Dy",0.5)
+            cmds.connectAttr(pma+".output2Dx",follicle_shapes+".parameterU")
+            cmds.connectAttr(pma+".output2Dy",follicle_shapes+".parameterV")
+            #counter double transform
+            cmds.connectAttr(circle_ctrl[0]+".translate",MD01+".input1")
+            cmds.setAttr(MD01+".input2X",-1)
+            cmds.setAttr(MD01+".input2Y",-1)
+            cmds.setAttr(MD01+".input2Z",-1)
+            cmds.connectAttr(MD01+".output",sdk_grp+".translate",force=True)
+            
+            cmds.group(ctrl_grp,follicle,n=follicle[0]+"_Group")
+            cmds.select(cl=1)
+            
+            
+            
+            
+            
+            ###--------------------------------------------------------------###
+        
+
+
+######################################################################################       
 
 def showWindow():
     name = "CustomTools"
@@ -110,6 +211,8 @@ def showWindow():
     cmds.button(label="Zero_Out", w=200, c=Zeroout, bgc=(0, .25, .25))
     cmds.button(label="Follicle at Each Pivot", w=200, c=follicleAtPivot, bgc=(0, .25, .25))
     cmds.button(label="Control at Each Pivot", w=200, c=controlatpivot, bgc=(0, .25, .25))
+    cmds.button(label="Create Follicle Control", w=200, c=follicelcontrol, bgc=(0, .25, .25))
+    
 
 
 showWindow()
